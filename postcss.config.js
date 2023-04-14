9f10263e3467b187487a4e6aa24a57496a339b20
Adewale Orotayo <didymus7007@gmail.com>
2023-04-14 16:46:55 +0100

    installed packages

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
+}
+
+// const tailwindcss = require('tailwindcss');
+// module.exports = {
+//     plugins: [
+//         tailwindcss('./tailwind.js'),
+//         require('autoprefixer')
+//     ],
+// };
