1ccd860a36bd1ba9f299df3b4b757e8a064390d4
Adewale Orotayo <didymus7007@gmail.com>
2025-10-17 12:47:07 +0100

    - changed logic to render conditionally

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Page from "./page";
 import { Footer } from "./components/footer";
-import { Route, Routes } from "react-router-dom";
 import { Contact } from "./components/pages/contact";
 import { Box, ChakraProvider, theme } from "@chakra-ui/react";
 import { Header, About, Portfolio, Experience } from "./components/pages";
@@ -10,14 +9,24 @@ const App = () => (
   <ChakraProvider theme={theme}>
     <Box bg="#282929" pb="20px" overflow="hidden">
       <Header />
-      <Routes>
-        <Route path="/" element={<Page />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Portfolio />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/contact" element={<Contact />} />
-        {/* <Route path="*" element={<ErrorPage />} /> */}
-      </Routes>
+      <main>
+        <section id="home">
+          <h1>Welcome to my portfolio</h1>
+          
+        </section>
+        <section id="about">
+          <About />
+        </section>
+        <section id="portfolio">
+          <Portfolio />
+        </section>
+        <section id="experience">
+          <Experience />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
+      </main>
       <Footer />
     </Box>
   </ChakraProvider>
